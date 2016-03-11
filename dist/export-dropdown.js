656b8dc51b0bc4d75d37623fb87d1e982e52039e
daruwanov <daruwanov@gmail.com>
2016-03-11 17:59:29 +0200

    Fix(CSV Report): Fix converting to object
    - Fix Create CSV report after PDF
    - add param for PDF view (landscape| portrait)

--- dist/export-dropdown.js
+++ dist/export-dropdown.js
@@ -1,97 +1,101 @@
-(function(window, document) {
+(function (window, document) {
     angular.module('ngExportButton', ['ngExportButton.directive']);
 
     angular.module('ngExportButton.directive', ['mgcrea.ngStrap', 'ngCsv', 'ngSanitize']);
 
-    angular.module('ngExportButton.directive').
-        directive('exportButton',['$modal',
-            function($modal) {
+    angular.module('ngExportButton.directive').directive('exportButton', ['$modal',
+        function ($modal) {
 
-                var linker = function(scope, element, attrs) {
+            var linker = function (scope, element, attrs) {
 
-                    //Init modal window instance
-                    var fileNameModal = $modal({scope: scope, template: 'templates/fileName-modal.tpl.html', show: false});
+                //Init modal window instance
+                var fileNameModal = $modal({scope: scope, template: 'templates/fileName-modal.tpl.html', show: false});
 
-                    //function that open modal window
-                    scope.showModal = function() {
-                        fileNameModal.$promise.then(fileNameModal.show);
-                    };
+                //function that open modal window
+                scope.showModal = function () {
+                    fileNameModal.$promise.then(fileNameModal.show);
+                };
 
-                    //function that create and return data for csv report.
-                    scope.getSCVArray = function() {
-                        var data = [];
-                        angular.forEach(scope.resources, function(resource) {
-                            var tmp = {}
-                            angular.forEach(scope.fields, function (field) {
-                                tmp[field] = resource[field];
-                            });
-                            data.push(tmp);
+
+                scope.CSVheaders = angular.copy(scope.headers);
+
+                //function that create and return data for csv report.
+                scope.getSCVArray = function () {
+                    var data = [];
+                    angular.forEach(scope.resources, function (resource) {
+                        var tmp = {}
+                        angular.forEach(scope.fields, function (field) {
+                            tmp[field] = resource[field];
                         });
-                        return data;
-                    };
+                        data.push(tmp);
+                    });
+                    return data;
+                };
 
-                    //function that create and return data for pdf table report.
-                    scope.getPDFDef = function(){
+                //function that create and return data for pdf table report.
+                scope.getPDFDef = function () {
 
-                        var data = [];
-                        data.push(scope.headers);
-                        angular.forEach(scope.resources, function(resource) {
-                            var tmp = []
-                            angular.forEach(scope.fields, function (field) {
-                                tmp.push(resource[field]);
-                            });
-                            data.push(tmp);
+                    var data = [];
+                    data.push(scope.headers);
+                    angular.forEach(scope.resources, function (resource) {
+                        var tmp = []
+                        angular.forEach(scope.fields, function (field) {
+                            tmp.push(resource[field]);
                         });
+                        data.push(tmp);
+                    });
 
 
-                        var pdf ={
-                            content: [
-                                {
-                                    table: {
-                                        headerRows: 1,
-                                        width: [ 'auto', 'auto', 'auto' ],
-                                        body: data
-                                    }
+                    var pdf = {
+                        content: [
+                            {
+                                table: {
+                                    headerRows: 1,
+                                    width: ['auto', 'auto', 'auto'],
+                                    body: data
                                 }
-                            ]
-                        };
-
-                        return pdf;
+                            }
+                        ],
+                        pageOrientation: scope.pageOrientation
                     };
 
-                    //function that download pdf file
-                    scope.downloadPDF = function(filename) {
+                    return pdf;
+                };
 
-                        var documentDef = scope.getPDFDef();
+                //function that download pdf file
+                scope.downloadPDF = function (filename) {
 
-                        pdfMake.createPdf(documentDef).download(filename+'.pdf');
+                    var documentDef = scope.getPDFDef();
 
-                    };
-
-                    //function that open pdf print dialog
-                    scope.printPDF = function() {
-
-                        var documentDef = scope.getPDFDef();
-
-                        pdfMake.createPdf(documentDef).print();
-                    };
+                    pdfMake.createPdf(documentDef).download(filename + '.pdf');
 
                 };
 
-                return {
-                    restrict: 'AE',
-                    scope: {
-                        fileName  : '=exportFileName',
-                        resources : '=resources',
-                        fields : '=fields',
-                        headers : '=headers'
-                    },
-                    link: linker,
-                    transclude: true,
-                    templateUrl: 'templates/export-button.tpl.html'
+                //function that open pdf print dialog
+                scope.printPDF = function () {
 
+                    var documentDef = scope.getPDFDef();
+
+                    pdfMake.createPdf(documentDef).print();
                 };
-            }
-        ]);
+
+            };
+
+            return {
+                restrict: 'AE',
+                scope: {
+                    fileName: '=exportFileName',
+                    resources: '=resources',
+                    fields: '=fields',
+                    headers: '=headers',
+                    pageOrientation: '=pageOrientation'
+                },
+                link: linker,
+                transclude: true,
+                templateUrl: 'templates/export-button.tpl.html'
+
+            };
+        }
+    ]);
 
 })(window, document);
